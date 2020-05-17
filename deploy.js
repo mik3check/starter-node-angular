e8873de07fd42dfba80b385d1c473155eb98723c
Michael Alexander <michaela171@gmail.com>
2020-05-17 17:58:01 -0500

    travis test 8

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.55.211.50',
+      host: '3.91.57.71',
       username: 'ubuntu',
       privateKey: 'snap-key.pem'
     })
